c7dcec4b96a9f93167a0af715ed284f59420a67a
Nikolai Tikhomirov <79159489230@yandex.ru>
2024-06-11 22:27:55 +0300

    HW5 update

--- HW5/src/main/java/ru/gb/springdemo/Application.java
+++ HW5/src/main/java/ru/gb/springdemo/Application.java
@@ -11,15 +11,6 @@ import ru.gb.springdemo.repository.ReaderRepository;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		BookRepository bookRepository = context.getBean(BookRepository.class);
-		ReaderRepository readerRepository = context.getBean(ReaderRepository.class);
-
-		bookRepository.save(new Book("война и мир"));
-		bookRepository.save(new Book("метрвые души"));
-		bookRepository.save(new Book("чистый код"));
-
-		readerRepository.save(new Reader("Игорь"));
-
+		SpringApplication.run(Application.class, args);
 	}
 }
