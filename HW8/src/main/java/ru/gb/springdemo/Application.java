2d14b5a04ed39abe4c9616551dac35af7c770549
Nikolai Tikhomirov <79159489230@yandex.ru>
2024-06-22 21:19:02 +0300

    HW8 start

---
+++ HW8/src/main/java/ru/gb/springdemo/Application.java
@@ -0,0 +1,16 @@
+package ru.gb.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.gb.springdemo.model.Book;
+import ru.gb.springdemo.model.Reader;
+import ru.gb.springdemo.repository.BookRepository;
+import ru.gb.springdemo.repository.ReaderRepository;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ HW8/src/main/java/ru/gb/springdemo/model/Reader.java
@@ -0,0 +1,51 @@
+package ru.gb.springdemo.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name = "readers")
+@Data
+@RequiredArgsConstructor
+@Schema(name = "Читатель")
+public class Reader {
+
+  public static long sequence = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Schema(name = "Идентификатор")
+  private final long id;
+
+  @Column(name = "name")
+  @Schema(name = "Имя читателя")
+  private final String name;
+
+  @Column(name = "login")
+  private final String login;
+
+  @Column(name = "password")
+  private final String password;
+
+  @Column(name = "role")
+  private final String role;
+
+  public Reader() {
+    this(sequence++, "unknown", "unknown", "unknown", "unknown");
+  }
+
+  public Reader(String name) {
+    this(sequence++, name, name, name, name);
+  }
+
+  public Reader(String name, String role) {
+    this(sequence++, name, name, name, role);
+  }
+
+  public Reader(String name, String login, String password, String role) {
+    this(sequence++, name, login, password, role);
+  }
+
+}
